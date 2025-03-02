81c8fe2e1e0a44ec64efc6bd7e599bc0ea8e9517
Shaeem Uddin <shaeemuddin25@gmail.com>
2025-03-03 00:58:33 +0600

    2nd commit

---
+++ sandbox.js
@@ -0,0 +1,86 @@
+// let age = 25;
+
+// let year = 1990;
+
+// let email = 'hello45@gmail.com';
+
+// console.log(email);
+
+// console.log(year,age);
+
+// age= 45;
+
+// console.log(age);
+
+// let firstName= 'Shaeem';
+
+// let lastName= 'Uddin';
+
+// let fullName = firstName + " "+ lastName;
+
+// console.log(fullName);
+
+// console.log(fullName.toUpperCase());
+
+// let id = fullName.toLowerCase();
+
+// console.log(id);
+
+// let index = email.indexOf('@');
+
+// console.log(index);
+
+// let result = email.lastIndexOf('l');
+
+// let result = email.slice(0,6);
+
+// let result = email.substr(2,5);
+
+// console.log(result);
+
+// let result = `My name is ${fullName} and my age is ${age}.`;
+
+// console.log(result);
+
+// let names = ['Shaeem', 'Ovi', 'Seam'];
+
+// let age = 30;
+
+//names[2] = 'frank';
+
+//console.log(names[2]);
+
+//let result = names.join(',');
+
+//let result = names.concat(['Maruf', 'Labib']);
+
+//let result = names.includes('Shaeem');
+
+//console.log(age ==30);
+//console.log(age !=30);
+
+
+// console.log(result);
+
+// let name = 'Shaeem';
+
+// console.log(name == 'Shaeem');
+// console.log(name == 'shaeem');
+// console.log(name > 'Mahi');
+// console.log(name < 'Mahi');
+// console.log(name < 'shaeem');
+
+//loose comparison == , !=
+//strick comparison === , !==
+
+// type conversion
+
+let score = '100';
+
+// score = Number(score);
+
+// console.log(score + 1);
+
+// console.log(typeof score);
+
+
